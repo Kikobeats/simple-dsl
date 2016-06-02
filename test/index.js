8cc7818c6b9c4f20450c3109d4f171b8659c96ea
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-06-02 21:29:00 +0200

    Removed unnecesary tests

--- test/index.js
+++ test/index.js
@@ -60,35 +60,4 @@ describe('simple-dsl', function () {
       })
     })
   })
-
-  // it('simple key value', function () {
-  //   dsl('kiko:beats').should.be.eql({kiko: 'beats'})
-  // })
-  //
-  // it('more than one key ', function () {
-  //   dsl('kiko:beats beats:kiko').should.be.eql({kiko: 'beats', beats: 'kiko'})
-  // })
-  //
-  // it('detect primary text', function () {
-  //   dsl('is:issue status:closed bug fixed').should.be.eql({
-  //     is: 'issue',
-  //     status: 'closed',
-  //     text: 'bug fixed'
-  //   })
-  // })
-
-  // it('array values', function () {
-  //   dsl('error in:title,body').should.be.eql({
-  //     text: 'error',
-  //     in: ['tile', 'body']
-  //   })
-  // })
-
-// it('handle spaces', function () {
-//   dsl('user:"kiko beats" status:closed bug fixed').should.be.eql({
-//     user: 'kiko beats',
-//     status: 'closed',
-//     text: 'bug fixed'
-//   })
-// })
 })
